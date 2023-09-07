f9d255939f304de28b2d9a2fc7294ca85358b145
Nguyen Xuan Hung <nguyenxuan.hung@rights-s.jp>
2023-09-07 14:05:20 +0700

    add: user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
